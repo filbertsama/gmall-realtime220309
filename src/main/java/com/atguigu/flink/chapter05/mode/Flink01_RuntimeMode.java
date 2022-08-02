fd5a6df285a71684bbad32b214f2acf66e8ca697
lzc <lzc@atguigu.cn>
2022-08-02 15:34:05 +0800

    update

---
+++ src/main/java/com/atguigu/flink/chapter05/mode/Flink01_RuntimeMode.java
@@ -0,0 +1,55 @@
+package com.atguigu.flink.chapter05.mode;
+
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @Author lzc
+ * @Date 2022/8/2 15:23
+ */
+public class Flink01_RuntimeMode {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.setInteger("rest.port", 2000);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        env.setParallelism(1);
+        
+        env.setRuntimeMode(RuntimeExecutionMode.BATCH);
+//        env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
+    
+        env
+//            .readTextFile("input/words.txt")
+            .socketTextStream("hadoop162", 9999)
+            .flatMap(new FlatMapFunction<String, Tuple2<String, Long>>() {
+                @Override
+                public void flatMap(String line,
+                                    Collector<Tuple2<String, Long>> out) throws Exception {
+                    for (String word : line.split(" ")) {
+                    
+                        out.collect(Tuple2.of(word, 1L));
+                    }
+                
+                }
+            })
+            .keyBy(t -> t.f0)
+            .sum(1)
+            .print();
+            
+        
+        
+        
+        try {
+            env.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+/*
+如果是有界的source: 可以使用流模式也可以使用批模式
+如果是无界的source: 只能使用流模式
+ */
